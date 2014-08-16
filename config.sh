c116951e265575be42a8fddb353311d6e2e83774
Agontuk <rifat662@gmail.com>
2014-08-16 17:04:13 +0600

    Use FirefoxSTE as the GITREPO

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
+GITREPO=${GITREPO:-"git://github.com/FirefoxSTE/b2g-manifest"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
